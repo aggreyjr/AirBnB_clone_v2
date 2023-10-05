baa71fe90b60dd6458a87c0a2dff942f5a932ced
aggreyjr <tukashabaaggrey89@gmail.com>
2023-10-05 06:01:27 -0700

    oh

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["54.160.85.72", "35.175.132.106"]
+env.hosts = ["100.26.178.187", "54.236.8.6"]
 
 
 def do_deploy(archive_path):
